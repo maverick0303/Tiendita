24e09c6e0ab485cf34ce3ec28fd88ebcd62c2dbc
AlfredoEstay02 <alfr.estay@duocuc.cl>
2023-08-24 15:23:55 -0400

    Validaciones listas,registro(rut,contraseñas)

--- src/app/pages/inicio-sesion/inicio-sesion.page.ts
+++ src/app/pages/inicio-sesion/inicio-sesion.page.ts
@@ -6,10 +6,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./inicio-sesion.page.scss'],
 })
 export class InicioSesionPage implements OnInit {
+  email: string = ''; // Inicialización de la variable email
+  password: string = ''; // Inicialización de la variable password
 
   constructor() { }
 
   ngOnInit() {
   }
 
+  onLogin() {
+    if (this.email && this.password) {
+      // Aquí puedes agregar la lógica para el inicio de sesión
+      console.log('Iniciar sesión con:', this.email, this.password);
+    }
+  }
 }
--- src/app/pages/registro/registro.page.ts
+++ src/app/pages/registro/registro.page.ts
@@ -6,10 +6,35 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./registro.page.scss'],
 })
 export class RegistroPage implements OnInit {
+  email: string = ''; // Inicialización de la variable email
+  password: string = ''; // Inicialización de la variable password
+  nombreUValue: string = '';
+  apellidoUValue: string = '';
+  rutUValue: string = '';
+  respuestaUValue: string = '';
+  preguntaUValue: string = '';
+  confirmPassword: string = '';
 
   constructor() { }
 
   ngOnInit() {
   }
 
+  onLogin() {
+    if (this.email && this.password) {
+      // Aquí puedes agregar la lógica para el inicio de sesión
+      console.log('Iniciar sesión con:', this.email, this.password);
+    }
+  }
+
+  validateRutU(rut: string): boolean {
+    if (rut === "") {
+      return false;
+    } else {
+      return /^\d{1,2}\.\d{3}\.\d{3}-[\dkK]$/.test(rut);
+    }
+  }
+  validatePasswordMatch(): boolean {
+    return this.password === this.confirmPassword;
+  }
 }
