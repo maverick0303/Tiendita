86fe76e2f95560ec3d353bfec9b881c6ca6caa58
maria <ma.yeguez@duocuc.cl>
2023-08-25 17:12:05 -0400

    AHORA ESTAN EN SUBCARPETAS

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -13,31 +13,27 @@ const routes: Routes = [
   },
   {
     path: 'inicio-sesion',
-    loadChildren: () => import('./pages/inicio-sesion/inicio-sesion.module').then( m => m.InicioSesionPageModule)
+    loadChildren: () => import('./pages/inicio/inicio-sesion/inicio-sesion.module').then( m => m.InicioSesionPageModule)
   },
   {
     path: 'registro',
-    loadChildren: () => import('./pages/registro/registro.module').then( m => m.RegistroPageModule)
+    loadChildren: () => import('./pages/inicio/registro/registro.module').then( m => m.RegistroPageModule)
   },
   {
     path: 'carrito',
-    loadChildren: () => import('./pages/carrito/carrito.module').then( m => m.CarritoPageModule)
+    loadChildren: () => import('./pages/Shop/carrito/carrito.module').then( m => m.CarritoPageModule)
   },
   {
     path: 'tienda',
-    loadChildren: () => import('./pages/tienda/tienda.module').then( m => m.TiendaPageModule)
-  },
-  {
-    path: 'articulo1',
-    loadChildren: () => import('./pages/articulo1/articulo1.module').then( m => m.Articulo1PageModule)
+    loadChildren: () => import('./pages/Shop/tienda/tienda.module').then( m => m.TiendaPageModule)
   },
   {
     path: 'datos-personales',
-    loadChildren: () => import('./pages/datos-personales/datos-personales.module').then( m => m.DatosPersonalesPageModule)
+    loadChildren: () => import('./pages/inicio/datos-personales/datos-personales.module').then( m => m.DatosPersonalesPageModule)
   },
   {
     path: 'recuperar-clave',
-    loadChildren: () => import('./pages/recuperar-clave/recuperar-clave.module').then( m => m.RecuperarClavePageModule)
+    loadChildren: () => import('./pages/inicio/recuperar-clave/recuperar-clave.module').then( m => m.RecuperarClavePageModule)
   },
   {
     path: 'menu-admin',
@@ -45,7 +41,7 @@ const routes: Routes = [
   },
   {
     path: 'reloj-ana',
-    loadChildren: () => import('./pages/reloj-ana/reloj-ana.module').then( m => m.RelojAnaPageModule)
+    loadChildren: () => import('./pages/categoria/reloj-ana/reloj-ana.module').then( m => m.RelojAnaPageModule)
   },
 
   {
@@ -54,146 +50,123 @@ const routes: Routes = [
   },
   {
     path: 'agregar-p-admin',
-    loadChildren: () => import('./pages/agregar-p-admin/agregar-p-admin.module').then( m => m.AgregarPAdminPageModule)
+    loadChildren: () => import('./pages/admin/agregar-p-admin/agregar-p-admin.module').then( m => m.AgregarPAdminPageModule)
   },
   {
     path: 'editar-p-admin',
-    loadChildren: () => import('./pages/editar-p-admin/editar-p-admin.module').then( m => m.EditarPAdminPageModule)
+    loadChildren: () => import('./pages/admin/editar-p-admin/editar-p-admin.module').then( m => m.EditarPAdminPageModule)
   },
   {
     path: 'relojdig',
-    loadChildren: () => import('./pages/relojdig/relojdig.module').then( m => m.RelojdigPageModule)
+    loadChildren: () => import('./pages/categoria/relojdig/relojdig.module').then( m => m.RelojdigPageModule)
   },
   {
     path: 'relojsma',
-    loadChildren: () => import('./pages/relojsma/relojsma.module').then( m => m.RelojsmaPageModule)
+    loadChildren: () => import('./pages/categoria/relojsma/relojsma.module').then( m => m.RelojsmaPageModule)
   },
   {
     path: 'audiina',
-    loadChildren: () => import('./pages/audiina/audiina.module').then( m => m.AudiinaPageModule)
+    loadChildren: () => import('./pages/categoria/audiina/audiina.module').then( m => m.AudiinaPageModule)
   },
   {
     path: 'audigamer',
-    loadChildren: () => import('./pages/audigamer/audigamer.module').then( m => m.AudigamerPageModule)
-  },  {
-    path: 'ag1',
-    loadChildren: () => import('./pages/ag1/ag1.module').then( m => m.Ag1PageModule)
+    loadChildren: () => import('./pages/categoria/audigamer/audigamer.module').then( m => m.AudigamerPageModule)
   },
   {
-    path: 'ag2',
-    loadChildren: () => import('./pages/ag2/ag2.module').then( m => m.Ag2PageModule)
+    path: 'ag1',
+    loadChildren: () => import('./pages/productos/ag1/ag1.module').then( m => m.Ag1PageModule)
   },
   {
     path: 'ag3',
-    loadChildren: () => import('./pages/ag3/ag3.module').then( m => m.Ag3PageModule)
+    loadChildren: () => import('./pages/productos/ag3/ag3.module').then( m => m.Ag3PageModule)
   },
   {
     path: 'ag4',
-    loadChildren: () => import('./pages/ag4/ag4.module').then( m => m.Ag4PageModule)
+    loadChildren: () => import('./pages/productos/ag4/ag4.module').then( m => m.Ag4PageModule)
   },
   {
     path: 'ag5',
-    loadChildren: () => import('./pages/ag5/ag5.module').then( m => m.Ag5PageModule)
-  },
-  {
-    path: 'ag6',
-    loadChildren: () => import('./pages/ag6/ag6.module').then( m => m.Ag6PageModule)
+    loadChildren: () => import('./pages/productos/ag5/ag5.module').then( m => m.Ag5PageModule)
   },
   {
     path: 'ai1',
-    loadChildren: () => import('./pages/ai1/ai1.module').then( m => m.Ai1PageModule)
+    loadChildren: () => import('./pages/productos/ai1/ai1.module').then( m => m.Ai1PageModule)
   },
   {
     path: 'ai2',
-    loadChildren: () => import('./pages/ai2/ai2.module').then( m => m.Ai2PageModule)
+    loadChildren: () => import('./pages/productos/ai2/ai2.module').then( m => m.Ai2PageModule)
   },
   {
     path: 'ai3',
-    loadChildren: () => import('./pages/ai3/ai3.module').then( m => m.Ai3PageModule)
+    loadChildren: () => import('./pages/productos/ai3/ai3.module').then( m => m.Ai3PageModule)
   },
   {
     path: 'ai4',
-    loadChildren: () => import('./pages/ai4/ai4.module').then( m => m.Ai4PageModule)
+    loadChildren: () => import('./pages/productos/ai4/ai4.module').then( m => m.Ai4PageModule)
   },
   {
     path: 'ai5',
-    loadChildren: () => import('./pages/ai5/ai5.module').then( m => m.Ai5PageModule)
+    loadChildren: () => import('./pages/productos/ai5/ai5.module').then( m => m.Ai5PageModule)
   },
   {
     path: 'ai6',
-    loadChildren: () => import('./pages/ai6/ai6.module').then( m => m.Ai6PageModule)
-  },
-  {
-    path: 'rd1',
-    loadChildren: () => import('./pages/rd1/rd1.module').then( m => m.Rd1PageModule)
+    loadChildren: () => import('./pages/productos/ai6/ai6.module').then( m => m.Ai6PageModule)
   },
   {
     path: 'rd2',
-    loadChildren: () => import('./pages/rd2/rd2.module').then( m => m.Rd2PageModule)
-  },
-  {
-    path: 'rd3',
-    loadChildren: () => import('./pages/rd3/rd3.module').then( m => m.Rd3PageModule)
+    loadChildren: () => import('./pages/productos/rd2/rd2.module').then( m => m.Rd2PageModule)
   },
   {
     path: 'rd4',
-    loadChildren: () => import('./pages/rd4/rd4.module').then( m => m.Rd4PageModule)
+    loadChildren: () => import('./pages/productos/rd4/rd4.module').then( m => m.Rd4PageModule)
   },
   {
     path: 'rd5',
-    loadChildren: () => import('./pages/rd5/rd5.module').then( m => m.Rd5PageModule)
+    loadChildren: () => import('./pages/productos/rd5/rd5.module').then( m => m.Rd5PageModule)
   },
   {
     path: 'rd6',
-    loadChildren: () => import('./pages/rd6/rd6.module').then( m => m.Rd6PageModule)
-  },
-  {
-    path: 'r1',
-    loadChildren: () => import('./pages/r1/r1.module').then( m => m.R1PageModule)
-  },
-  {
-    path: 'r2',
-    loadChildren: () => import('./pages/r2/r2.module').then( m => m.R2PageModule)
+    loadChildren: () => import('./pages/productos/rd6/rd6.module').then( m => m.Rd6PageModule)
   },
   {
     path: 'r3',
-    loadChildren: () => import('./pages/r3/r3.module').then( m => m.R3PageModule)
+    loadChildren: () => import('./pages/productos/r3/r3.module').then( m => m.R3PageModule)
   },
   {
     path: 'r4',
-    loadChildren: () => import('./pages/r4/r4.module').then( m => m.R4PageModule)
+    loadChildren: () => import('./pages/productos/r4/r4.module').then( m => m.R4PageModule)
   },
   {
     path: 'r5',
-    loadChildren: () => import('./pages/r5/r5.module').then( m => m.R5PageModule)
+    loadChildren: () => import('./pages/productos/r5/r5.module').then( m => m.R5PageModule)
   },
   {
     path: 'r6',
-    loadChildren: () => import('./pages/r6/r6.module').then( m => m.R6PageModule)
+    loadChildren: () => import('./pages/productos/r6/r6.module').then( m => m.R6PageModule)
   },
   {
     path: 'rs1',
-    loadChildren: () => import('./pages/rs1/rs1.module').then( m => m.Rs1PageModule)
+    loadChildren: () => import('./pages/productos/rs1/rs1.module').then( m => m.Rs1PageModule)
   },
   {
     path: 'rs2',
-    loadChildren: () => import('./pages/rs2/rs2.module').then( m => m.Rs2PageModule)
+    loadChildren: () => import('./pages/productos/rs2/rs2.module').then( m => m.Rs2PageModule)
   },
   {
     path: 'rs3',
-    loadChildren: () => import('./pages/rs3/rs3.module').then( m => m.Rs3PageModule)
+    loadChildren: () => import('./pages/productos/rs3/rs3.module').then( m => m.Rs3PageModule)
   },
   {
     path: 'rs4',
-    loadChildren: () => import('./pages/rs4/rs4.module').then( m => m.Rs4PageModule)
+    loadChildren: () => import('./pages/productos/rs4/rs4.module').then( m => m.Rs4PageModule)
   },
   {
     path: 'rs5',
-    loadChildren: () => import('./pages/rs5/rs5.module').then( m => m.Rs5PageModule)
+    loadChildren: () => import('./pages/productos/rs5/rs5.module').then( m => m.Rs5PageModule)
   },
   {
     path: 'rs6',
-    loadChildren: () => import('./pages/rs6/rs6.module').then( m => m.Rs6PageModule)
+    loadChildren: () => import('./pages/productos/rs6/rs6.module').then( m => m.Rs6PageModule)
   },
 
 
