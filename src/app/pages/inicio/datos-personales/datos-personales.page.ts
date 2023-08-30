9b85c47f1d7b7894e20b3161a319d70ed6ad9a54
AlfredoEstay02 <alfr.estay@duocuc.cl>
2023-08-29 23:04:53 -0400

    ta raro esto pero funciona jajajaja

--- src/app/pages/inicio/datos-personales/datos-personales.page.ts
+++ src/app/pages/inicio/datos-personales/datos-personales.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-datos-personales',
@@ -6,14 +7,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./datos-personales.page.scss'],
 })
 export class DatosPersonalesPage implements OnInit {
-  nombreValue: string = '';
-  apellidoValue: string = '';
-  segPersonaValue: string = '';
+  nombreValue: string = 'Maria ';
+  apellidoValue: string = 'Yeguez';
+  segPersonaValue: string = 'Ernesto Pino';
+  correo: string = '';  // Variable para almacenar el correo
 
-  constructor() { }
+  constructor(private route: ActivatedRoute) { }
 
   ngOnInit() {
+    this.route.queryParams.subscribe((params: any) => {
+      this.correo = params['correo'];  // Obtener el correo electrónico
+    });
   }
+
   onKeyDown(event: KeyboardEvent) {
     if (event.key === ' ') {
       event.preventDefault(); // Evita que la barra espaciadora sea escrita en el campo
--- src/app/pages/inicio/inicio-sesion/inicio-sesion.page.ts
+++ src/app/pages/inicio/inicio-sesion/inicio-sesion.page.ts
@@ -1,11 +1,7 @@
 import { Component } from '@angular/core';
-//animaciones:
-import { FormControl, Validators, FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { Router, NavigationExtras } from '@angular/router';
 import { ToastController } from '@ionic/angular';
-
-//
-
+import { FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-inicio-sesion',
@@ -14,54 +10,48 @@ import { ToastController } from '@ionic/angular';
 })
 export class InicioSesionPage {
   rol: number = 0;
-  gmail: string = "";
-  password: string = "";
-
+  gmail: string = '';
+  password: string = '';
 
   hide = true;
   email = new FormControl('', [Validators.required, Validators.email]);
+
+  constructor(private router: Router, private toastController: ToastController) { }
 
   getErrorMessage() {
     if (this.email.hasError('required')) {
       return 'Debes ingresar un email:)';
     }
-
     return this.email.hasError('email') ? 'No es un email valido' : '';
   }
+
   async presentToast() {
     const toast = await this.toastController.create({
       message: 'Los datos proporcionados no coinciden',
       duration: 950,
       position: 'middle',
-
     });
-
     await toast.present();
   }
 
-  
-  inicio_sesion () {
-    if (this.gmail == "admin@gmail.com" && this.password == "Admin123.") {
-      //poner redireccion
-      this.rol = 2;
-      
-    } else if (this.gmail == "usuario@gmail.com" && this.password == "Usuario123.") {
-      this.rol = 1;
-      
-    }
-    else {
-      // poner mensaje de error
+  inicio_sesion() {
+    if ((this.gmail === 'admin@gmail.com' && this.password === 'Admin123.') ||
+        (this.gmail === 'usuario@gmail.com' && this.password === 'Usuario123.')) {
+      this.rol = (this.gmail === 'admin@gmail.com') ? 2 : 1;
+      this.irADatosPersonales(this.gmail);
+    } else {
       this.presentToast();
       return;
     }
-    let navigationExtras: NavigationExtras = {
-      state:{
-        roles: this.rol
-      }
-      
-    }
-    this.router.navigate(['/tienda'], navigationExtras);
   }
-  constructor(private router: Router, private toastController: ToastController, ) { }
 
+  irADatosPersonales(correo: string) {
+    const navigationExtras: NavigationExtras = {
+      state: {
+        roles: this.rol,
+        correo: correo,
+      },
+    };
+    this.router.navigate(['/datos-personales'], navigationExtras);
+  }
 }
