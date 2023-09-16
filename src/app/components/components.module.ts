f8b5d4f7d256ccc920266c35829f1d6f347c5aa2
maria <ma.yeguez@duocuc.cl>
2023-09-16 18:18:39 -0300

    SE COLOCARON MENUES EN TODOS LOS HTML

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -1,5 +1,6 @@
 import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 import { Componente1Component } from './componente1/componente1.component';
+import { Componente2Component } from './componente2/componente2.component';
 //cosa del carrito:
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
@@ -12,8 +13,8 @@ import { FormsModule } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
 
 @NgModule({
-    declarations: [Componente1Component],
-    exports: [Componente1Component],
+    declarations: [Componente1Component,Componente2Component],
+    exports: [Componente1Component,Componente2Component],
     schemas: [CUSTOM_ELEMENTS_SCHEMA],
     imports: [MatBadgeModule, MatButtonModule, MatIconModule, RouterModule, IonicModule, FormsModule, CommonModule]
 })
