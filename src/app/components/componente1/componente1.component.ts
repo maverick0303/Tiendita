6af3e47e5172f389497006e1ef01bfacde7becf9
maria <ma.yeguez@duocuc.cl>
2023-10-04 18:27:24 -0300

    cambios juju

--- src/app/components/componente1/componente1.component.ts
+++ src/app/components/componente1/componente1.component.ts
@@ -1,14 +1,23 @@
 import { Component, OnInit } from '@angular/core';
+import { CarritoService } from 'src/app/services/carrito.service';
 
 @Component({
   selector: 'app-componente1',
   templateUrl: './componente1.component.html',
   styleUrls: ['./componente1.component.scss'],
 })
-export class Componente1Component  implements OnInit {
+export class Componente1Component implements OnInit {
+  productosEnCarrito: any[] = [];
+  cantidad: number = 0; // Inicializa la cantidad en 0
 
-  constructor() { }
+  constructor(private carritoService: CarritoService) {}
 
-  ngOnInit() {}
-
+  ngOnInit() {
+    this.productosEnCarrito = this.carritoService.getProductosEnCarrito();
+    
+    // Suscribirse al observable cantidad$ para recibir actualizaciones
+    this.carritoService.cantidad$.subscribe((cantidad) => {
+      this.cantidad = cantidad;
+    });
+  }
 }
--- src/app/pages/Shop/carrito/carrito.page.ts
+++ src/app/pages/Shop/carrito/carrito.page.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { CarritoService } from 'src/app/services/carrito.service'; // Importa el servicio del carrito
+import { CarritoService } from 'src/app/services/carrito.service';
 
 @Component({
   selector: 'app-carrito',
@@ -7,25 +7,91 @@ import { CarritoService } from 'src/app/services/carrito.service'; // Importa el
   styleUrls: ['./carrito.page.scss'],
 })
 export class CarritoPage implements OnInit {
-  productosEnCarrito: any[] = []; // Declara la propiedad para los productos en el carrito
+  productosEnCarrito: any[] = [];
+  cantidad: number = 1;
 
   constructor(private carritoService: CarritoService) { }
 
   ngOnInit() {
-    this.productosEnCarrito = this.carritoService.getProductosEnCarrito(); // Obtiene los productos del carrito
+    this.productosEnCarrito = this.carritoService.getProductosEnCarrito();
+    this.cantidad = this.carritoService.getCantidad();
   }
 
   calcularTotal(): number {
-    // Implementa la lógica para calcular el total de la compra
     let total = 0;
     for (const producto of this.productosEnCarrito) {
-      total += producto.precio;
+      total += producto.precioTotal; // Debes sumar el precioTotal en lugar del precio
     }
     return total;
   }
 
   vaciarCarrito() {
-    this.carritoService.vaciarCarrito(); // Llama al método para vaciar el carrito del servicio
-    this.productosEnCarrito = []; // Limpia la lista de productos en el carrito
+    this.carritoService.vaciarCarrito();
+    this.productosEnCarrito = [];
+    this.cantidad = 1; // Restablece la cantidad a 1 al vaciar el carrito
   }
+
+  aumentarCantidad(producto: any) {
+    if (producto.stock > this.cantidad) {
+      // Si hay suficiente stock, aumenta la cantidad en el carrito
+      this.cantidad++;
+  
+      // Actualiza el precio total del producto
+      producto.precioTotal = producto.precio * this.cantidad;
+  
+      // Actualiza la cantidad en el servicio
+      this.carritoService.setCantidad(this.cantidad);
+    } else {
+      // Muestra un mensaje de que no hay suficiente stock
+      console.log('No hay suficiente stock disponible.');
+    }
+  }
+
+  disminuirCantidad(producto: any) {
+    if (this.cantidad > 1) {
+      // Disminuye la cantidad en el carrito si no es menor que 1
+      this.cantidad--;
+  
+      // Actualiza el precio total del producto
+      producto.precioTotal = producto.precio * this.cantidad;
+  
+      // Actualiza la cantidad en el servicio
+      this.carritoService.setCantidad(this.cantidad);
+    } else if (this.cantidad === 1) {
+      // Si la cantidad es 1, elimina el producto del carrito
+      this.eliminarProducto(producto);
+    }
+  }
+  
+  eliminarProducto(producto: any) {
+    // Elimina el producto del carrito
+    const index = this.productosEnCarrito.indexOf(producto);
+    if (index > -1) {
+      this.productosEnCarrito.splice(index, 1);
+      // Actualiza la cantidad en el servicio después de eliminar un producto
+      this.carritoService.setCantidad(this.cantidad);
+    }
+  }  
+  
+  agregarAlCarrito(producto: any) {
+    const cantidadSeleccionada = this.cantidad;
+    
+    // Verificar si el producto ya está en el carrito
+    const productoExistente = this.productosEnCarrito.find(p => p.id === producto.id);
+  
+    if (productoExistente) {
+      // Si el producto ya existe en el carrito, actualiza su cantidad
+      productoExistente.cantidad += cantidadSeleccionada;
+    } else {
+      // Si el producto no existe en el carrito, agrégalo
+      this.carritoService.agregarAlCarrito(producto, cantidadSeleccionada);
+    }
+  
+    // Restablece la cantidad a 1 después de agregar al carrito
+    this.cantidad = 1;
+  
+    // Actualiza la cantidad en el servicio
+    this.carritoService.setCantidad(this.cantidad);
+  }
+  
 }
