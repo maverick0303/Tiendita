fc4fa764431152fefd3173c2a9d4ca8f77c26d1f
maria <ma.yeguez@duocuc.cl>
2023-11-03 11:44:00 -0300

    Se genero el icono, la firma y la APK

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
-  appName: 'Tienda',
+  appName: 'TimeTech ',
   webDir: 'www',
   server: {
     androidScheme: 'https'
